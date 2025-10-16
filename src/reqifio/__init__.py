4b47f23f9ac760445078191004db91e234c10031
jrighett <jrighetti@anduril.com>
2025-10-16 16:59:06 -0700

    wip

--- src/reqifio/__init__.py
+++ src/reqifio/__init__.py
@@ -5,6 +5,7 @@ This module initializes the reqif_lib package. We expose the primary modules:
  - reqif_parser: Parsing ReqIF files (XML) into internal model.
  - reqif_writer: Writing internal model to ReqIF XML files.
  - sqlite_adapter: Conversion of internal model to a full SQL representation and vice versa.
+ - csv_adapter: Conversion of internal model to a full CSV representation and vice versa.
  - command: Command pattern implementation for undo/redo of CRUD operations.
  - model: Internal data model representing the ReqIF schema.
 
@@ -17,6 +18,7 @@ __all__ = [
     "reqif_parser",
     "reqif_writer",
     "sqlite_adapter",
+    "csv_adapter",
     "command",
     "model",
 ]
